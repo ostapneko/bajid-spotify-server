87cc4c0fbb815847b9307fb2f5bae7b62d9e1096
Thomas Franquelin <thomas@adalong.com>
2020-08-08 19:25:32 +0200

    Add login redirections

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"franquel.in/bajidspotifyserver/config"
 	"franquel.in/bajidspotifyserver/gcp"
@@ -14,6 +15,10 @@ func main() {
 	fmt.Println("Starting Bajid server!")
 
 	gcpProjectId := config.RequireEnvVar("GCP_PROJECT_ID")
+	spotifyClientID := config.RequireEnvVar("SPOTIFY_CLIENT_ID")
+	authorizedRedirectURI := config.RequireEnvVar("SPOTIFY_REDIRECT_URI")
+	loginRedirectURL := spotifyRedirectURL(spotifyClientID, authorizedRedirectURI)
+	log.Printf("Login redirect URL: %s\n", loginRedirectURL)
 
 	sm, err := gcp.NewSecretManager(gcpProjectId)
 
@@ -30,6 +35,7 @@ func main() {
 	}
 
 	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/login", http.RedirectHandler(loginRedirectURL, 303))
 
 	port := config.RequireEnvVar("PORT")
 	if port == "" {
@@ -41,4 +47,17 @@ func main() {
 	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 
 	log.Fatal(err)
+}
+
+const spotifyScopes = "user-read-private user-read-email"
+
+func spotifyRedirectURL(clientID string, redirectURI string) string {
+	encScopes := url.QueryEscape(spotifyScopes)
+	encRedirectURI := url.QueryEscape(redirectURI)
+
+	return "https://accounts.spotify.com/authorize" +
+		"?response_type=code" +
+		"&client_id=" + clientID +
+		"&scope=" + encScopes +
+		"&redirect_uri=" + encRedirectURI
 }
