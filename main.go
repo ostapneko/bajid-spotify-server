093f4d785267e4fb5128c1b84af110463ea569b9
Thomas Franquelin <thomas@adalong.com>
2020-08-08 17:03:24 +0200

    Add spotify secret from secrets manager

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 
 	"franquel.in/bajidspotifyserver/config"
+	"franquel.in/bajidspotifyserver/gcp"
 )
 
 func main() {
 	fmt.Println("Starting Bajid server!")
+
+	gcpProjectId := config.RequireEnvVar("GCP_PROJECT_ID")
+
+	sm, err := gcp.NewSecretManager(gcpProjectId)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ctx := context.Background()
+
+	_, err = sm.GetSecret(ctx, "SPOTIFY_CLIENT_SECRET")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	http.HandleFunc("/", handler)
 
@@ -20,7 +38,7 @@ func main() {
 
 	log.Printf("Bajid listening on port %s", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 
 	log.Fatal(err)
 }
